76d430c546998b988d4c3d88ccdc78fe2c0cca09
Romain Davaze <davaze.romain@gmail.com>
2017-11-09 17:30:10 +0100

    Implementing Glide

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -2,6 +2,10 @@ package fr.android.androidexercises;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -11,7 +15,9 @@ public class LibraryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_library);
 
         // TODO Get image view and load image form URL
-        // http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg
+        Glide.with(this)
+                .load("http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg")
+                .into((ImageView) findViewById(R.id.downloadImageView));
     }
 
 }
