045c0d288bcfddc3168298fad14afadfde2eb40b
Romain Davaze <davaze.romain@gmail.com>
2017-11-09 14:39:30 +0100

    Implementing Date and Time pickers

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -1,5 +1,7 @@
 package fr.android.androidexercises;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +10,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -20,6 +25,8 @@ public class LibraryActivity extends AppCompatActivity {
 
         Button openButton = (Button) findViewById(R.id.openButton);
 
+        Button dateButton = findViewById(R.id.dateButton);
+
         final Book book = new Book("Garry Whopper", "CK Rowling");
 
         openButton.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +36,26 @@ public class LibraryActivity extends AppCompatActivity {
                 // TODO Add book to intent
                 intent.putExtra("BOOK", book);
                 startActivity(intent);
+            }
+        });
+
+        dateButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(LibraryActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, final int year, final int month, final int dayOfMonth) {
+                        new TimePickerDialog(LibraryActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                Toast.makeText(LibraryActivity.this, dayOfMonth + "/" + (month+1) + "/" + year + " " + hourOfDay + ":" + minute,
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }, 14, 37, true).show();
+
+                    }
+                }, 2017, 10, 9).show();
             }
         });
     }
